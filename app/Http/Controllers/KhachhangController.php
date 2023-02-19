ef2ce94dd02252014828dc4b846aeeea146a8622
VanTuan <84395117+tuanbie@users.noreply.github.com>
2023-02-20 00:01:43 +0700

    first commit

---
+++ app/Http/Controllers/KhachhangController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use App\Models\khachhang;
+use App\Http\Resources\khachhang as KhachhangResource;
+use Illuminate\Support\Facades\Validator;
+
+class KhachhangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        $khachhangs = khachhang::select('ten', 'sdt', 'diachi')->get();
+        return response()->json($khachhangs);
+        
+
+        // $customers = khachhang::select('ten', 'sdt', 'diachi')->get();
+        // // $customers = khachhang::all();
+        // return response()->json($customers);
+        // // return redirect()->json($customers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): Response
+    {
+        $input = $request->all(); 
+        $validator = Validator::make($input, [
+        'ten' => 'required', 'sdt' => 'required','diachi' => 'required'
+        ]);
+        if($validator->fails()){
+            $arr = [
+            'success' => false,
+            'message' => 'Lỗi kiểm tra dữ liệu',
+            'data' => $validator->errors()
+            ];
+            return response()->json($arr, 200);
+        }
+        $product = khachhang::create($input);
+        $arr = ['status' => true,
+            'message'=>"Khách hàng đã lưu thành công",
+            'data'=> new KhachhangResource($product)
+        ];
+        return response()->json($arr, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): Response
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): Response
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): Response
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/khachhang.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class khachhang extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'ten' => $this->ten,
+            'sdt' => $this->sdt,
+            'diachi' => $this->diachi,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y'),
+          ];
+    }
+}
